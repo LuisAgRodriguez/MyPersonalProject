af879a6b385f0c959ae9915990f95bf799d942eb
Luis Rodrguez <ls2003rodriguez@gmail.com>
2025-04-16 14:33:06 -0300

    Los ultimos cambios de los proyectos

---
+++ 05-Admin-Dashboard/script.js
@@ -0,0 +1,26 @@
+const openMenu = document.querySelector(".open-menu");
+const closeMenu = document.querySelector(".open-close");
+const leftNavbar = document.querySelector(".left-navbar");
+
+const darkMode = document.querySelector("#light-dark");
+let tf = true;
+darkMode.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode-variable");
+    if(tf){
+        darkMode.textContent = "dark_mode";
+        tf = false;
+    } else {
+        darkMode.textContent = "light_mode";
+        tf = true;
+    }
+})
+
+
+openMenu.addEventListener("click", () => {
+    leftNavbar.style.display = "block";
+}) 
+
+closeMenu.addEventListener("click", () => {
+    leftNavbar.style.display = "none";
+}) 
+
